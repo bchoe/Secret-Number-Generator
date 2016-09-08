d07771b93206dfcd52d028f2e69d8b3d436d927e
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-07 19:56:21 -1000

    secret number shhhhhhh

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,9 @@
 'use strict';
 module.exports = function() {
-  
+
+var secretNumberGenerator=Math.floor((Math.random() * 1000000)+1);
+ /*return */function secretNumber(){
+    return secretNumberGenerator;
+  };
+  return secretNumber;
 };

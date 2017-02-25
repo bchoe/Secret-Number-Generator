8e365fcfc6bf96c93a953035eeabcb434d888ba8
Bryan Choe <bryan.choe88@gmail.com>
2017-02-24 15:17:42 -1000

    refactored code

--- secret_number.js
+++ secret_number.js
@@ -1,9 +1,16 @@
 'use strict';
 module.exports = function() {
 
-var secretNumberGenerator=Math.floor((Math.random() * 1000000)+1);
- /**/ function secretNumber(){
+var secretNumberGenerator = Math.floor((Math.random() * 1000000) + 1);
+
+console.log('secretNumberGenerator',secretNumberGenerator);
+
+  function secretNumber(){
+
     return secretNumberGenerator;
-  };
+
+  }
+
   return secretNumber;
+
 };
